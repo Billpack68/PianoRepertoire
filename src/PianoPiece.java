eaa49267db08feec146de038d129ebf10d108f01
Bill Killpack <trombonebill6912@gmail.com>
2025-10-16 20:59:06 -0600

    Created Author class

--- src/PianoPiece.java
+++ src/PianoPiece.java
@@ -1,7 +1,16 @@
 public class PianoPiece {
   private String name;
   private String author;
+  private String arranger;
   private String book;
   private String genre;
+
+  public PianoPiece(String name, String author, String arranger,
+                    String book, String genre) {
+    this.name=name;
+    this.author = author;
+    this.arranger = arranger;
+    this.book = book;
+    this.genre = genre;
+  }
 }
- 
