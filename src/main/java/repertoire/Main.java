3a78b9d9f18bb433fff2f99af2bee1e8545236a9
Bill Killpack <trombonebill6912@gmail.com>
2025-10-16 21:25:34 -0600

    Modified main

--- src/main/java/repertoire/Main.java
+++ src/main/java/repertoire/Main.java
@@ -14,5 +14,16 @@ public class Main {
     List<Genre> genres = new ArrayList<>();
 
     System.out.println("Welcome to the piano repertoire tracker!");
+    boolean running = true;
+
+    while (running) {
+      System.out.println("What would you like to do?");
+      System.out.println("1. Add piece");
+      System.out.println("2. Add composer");
+      System.out.println("3. Add arranger");
+      System.out.println("4. Add book");
+      System.out.println("5. Add genre");
+      System.out.println("6. Exit");
+    }
   }
 }
