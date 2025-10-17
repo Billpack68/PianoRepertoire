960c5f82e310453f2daf8caff7ff7c2269178f2b
Bill Killpack <trombonebill6912@gmail.com>
2025-10-16 21:18:37 -0600

    Modified main

--- src/main/java/repertoire/Main.java
+++ src/main/java/repertoire/Main.java
@@ -1,9 +1,18 @@
 package main.java.repertoire;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    
+    Scanner scanner = new Scanner(System.in);
+    List<PianoPiece> pianoPieces = new ArrayList<>();
+    List<Author> authors = new ArrayList<>();
+    List<Arranger> arrangers = new ArrayList<>();
+    List<Book> books = new ArrayList<>();
+    List<Genre> genres = new ArrayList<>();
+
+    System.out.println("Welcome to the piano repertoire tracker!");
   }
 }
